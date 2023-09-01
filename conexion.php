872e8a1b5d778c98d2a0fb558b2b629a66950227
405 <405@academicos.cesde.edu.co>
2023-09-01 11:42:23 -0500

    Creamos repo

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+$servidor="localhost";
+$usuario="root";
+$contrasena="";
+$basedatos="bdfactura";
+
+$conexion=new mysqli($servidor,$usuario,$contrasena,$basedatos);
+
+if($conexion->connect_errno)
+{
+    die("Error: ".$conexion->connect_errno);
+}
+
+?>
